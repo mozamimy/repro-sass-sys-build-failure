835cf07b91548e3695c037eca16f9c0ac3b4a5d9
mozamimy <alice@mozami.me>
2019-05-06 20:27:58 +0900

    Rename run.sh and enable --env arg

--- run_local.sh
+++ run_local.sh
@@ -10,6 +10,6 @@ exec docker run --rm \
   --memory 4096M \
   --volume ${PWD}:/build \
   --workdir /build \
-  # --env MAKE_LIBSASS_JOBS=36 \
+  --env MAKE_LIBSASS_JOBS=36 \
   rust:1.34-stretch \
   cargo build --release --locked -vv
